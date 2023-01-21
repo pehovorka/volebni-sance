0ce43ea2a00490745e266f364065f40da3c428a5
Petr Hovorka <pehovorka@gmail.com>
2023-01-21 22:28:53 +0100

    ✨ Add Firebase to web

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,10 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { db } from "@/utils/firebase";
+import { collection, doc, getDocs, Timestamp } from "firebase/firestore";
+import {
+  CollectionName,
+  ElectionRecord,
+} from "../../../importer/functions/src/interfaces/database";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+interface Props {
+  election: ElectionRecord[];
+}
+
+export default function Home({ election }: Props) {
   return (
     <>
       <Head>
@@ -18,8 +28,37 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>Hello!</h1>
+        <h1 className={styles.title}>Pavel vs. Babiš – prezident 2023</h1>
+        <code>
+          <pre>{JSON.stringify(election, ",", 2)}</pre>
+        </code>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const documents: any[] = [];
+  const querySnapshot = await getDocs(
+    collection(db, CollectionName.ElectionRecords)
+  );
+  querySnapshot.forEach((doc) => {
+    const data = doc.data() as Omit<ElectionRecord, "date"> & {
+      date: Timestamp;
+    };
+    documents.push({
+      ...data,
+      date: data.date.toDate().toISOString(),
+    });
+  });
+
+  return {
+    props: {
+      election: documents,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  };
 }
